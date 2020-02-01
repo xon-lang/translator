432871d580f5743f63723089979981dacd85646b
nizami <nizami.dev@gmail.com>
2020-02-01 05:45:17 +0300

    Update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { evalExpression, parseExpression } from '@xon/ast';
 
 test('check ast', () => {
-    const code = '3+3';
+    const code = '100 >> 4 |x: x^x + x |z: z^x && x^z | z + x';
     const tree = parseExpression(code);
     const result = evalExpression(tree);
 
