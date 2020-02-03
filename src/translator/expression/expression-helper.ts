9cfe1e7d38c496b7c63ef3ab127e30133c171e98
nizami <nizami.dev@gmail.com>
2020-02-03 05:31:58 +0300

    Fix AddSub instanceof issue

--- src/translator/expression/expression-helper.ts
+++ src/translator/expression/expression-helper.ts
@@ -1,12 +1,4 @@
-import {
-    AddSubExpressionTree,
-    ExpressionTree,
-    FunctionExpressionTree,
-    IdExpressionTree,
-    LiteralExpressionTree,
-    MemberExpressionTree,
-    RelationalExpressionTree,
-} from '@xon/ast';
+
 import { AddSubExpressionTranslator } from './add-sub-expression/add-sub-expression.translator';
 import { ExpressionTranslator } from './expression.translator';
 import { FunctionExpressionTranslator } from './function-expression/function-expression.translator';
@@ -14,6 +6,9 @@ import { IdExpressionTranslator } from './id-expression/id-expression.translator
 import { LiteralExpressionTranslator } from './literal-expression/literal-expression.translator';
 import { MemberExpressionTranslator } from './member-expression/member-expression.translator';
 import { RelationalExpressionTranslator } from './relational-expression/relational-expression.translator';
+import { ExpressionTree, IdExpressionTree, FunctionExpressionTree, LiteralExpressionTree, MemberExpressionTree, RelationalExpressionTree, AddSubExpressionTree } from '@xon/ast';
+
+
 
 export function getExpressionTranslator(tree: ExpressionTree): ExpressionTranslator {
     if (tree instanceof IdExpressionTree) return new IdExpressionTranslator(tree);
@@ -21,6 +16,5 @@ export function getExpressionTranslator(tree: ExpressionTree): ExpressionTransla
     if (tree instanceof LiteralExpressionTree) return new LiteralExpressionTranslator(tree);
     if (tree instanceof MemberExpressionTree) return new MemberExpressionTranslator(tree);
     if (tree instanceof RelationalExpressionTree) return new RelationalExpressionTranslator(tree);
-    console.log(tree.toJson());
-    if (tree.type =='AddSubExpression') return new AddSubExpressionTranslator(tree as AddSubExpressionTree);
+    if (tree instanceof AddSubExpressionTree) return new AddSubExpressionTranslator(tree);
 }
