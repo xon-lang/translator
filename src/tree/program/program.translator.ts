980e3d014403696fe84eea56ac87ab0722b8b361
nizami <nizami.dev@gmail.com>
2020-08-16 23:01:37 +0300

    Add input output tests

--- src/tree/program/program.translator.ts
+++ src/tree/program/program.translator.ts
@@ -10,41 +10,44 @@ export class ProgramTranslator extends BaseTranslator {
     }
 
     translate() {
+        if (!this.tree.statements.length || !this.tree.statements.length) return '';
 
-        this.scopes.push([])
+        this.scopes.push([]);
 
-        let statements = []
-        let functions = []
+        let statements = [];
+        let functions = [];
         for (const stmt of this.tree.statements.map(getStatementTranslator)) {
             if (stmt instanceof FunctionStatementTranslator) {
-                const exportOp = stmt.tree.value.name.startsWith('_') ? `` : 'export '
-                functions.push(`${exportOp}` + stmt.translate())
+                const exportOp = stmt.tree.value.name.startsWith('_') ? `` : 'export ';
+                functions.push(`${exportOp}` + stmt.translate());
             } else {
-                statements.push(stmt.translate())
-
+                statements.push(stmt.translate());
             }
         }
 
-        const definitions = this.tree.definitions.map(x => new DefinitionTranslator(x))
-            .map(x => x.translate());
+        const definitions = this.tree.definitions
+            .map((x) => new DefinitionTranslator(x))
+            .map((x) => x.translate());
 
-        let vars = ''
-        let export_vars = []
-        let private_vars = []
+        let vars = '';
+        let export_vars = [];
+        let private_vars = [];
         for (const v of this.scopes.pop()) {
             if (v.startsWith('_')) {
-                private_vars.push(v)
+                private_vars.push(v);
             } else {
-                export_vars.push(v)
+                export_vars.push(v);
             }
         }
         if (export_vars.length) {
-            vars += `export let ${export_vars.join(', ')};\n`
+            vars += `export let ${export_vars.join(', ')};\n`;
         }
         if (private_vars.length) {
-            vars += `let ${private_vars.join(', ')};`
+            vars += `let ${private_vars.join(', ')};`;
         }
 
-        return `${vars}\n${statements.join('\n')}\n\n${functions.join('\n\n')}\n\n${definitions.join('\n\n')}`;
+        return `${vars}\n${statements.join('\n')}\n\n${functions.join(
+            '\n\n'
+        )}\n\n${definitions.join('\n\n')}`;
     }
 }
