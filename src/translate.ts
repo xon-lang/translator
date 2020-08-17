7e178cfcf8bf0d08f3f119895c3429aa8be8ef3a
nizami <nizami.dev@gmail.com>
2020-08-17 22:11:51 +0300

    Add translateProgram method

--- src/translate.ts
+++ src/translate.ts
@@ -1,8 +1,15 @@
-import { parseExpression } from '@xon/ast';
+import { parseCode, parseExpression, ProgramTree } from '@xon/ast';
 import { getExpressionTranslator } from './tree/expression/expression-helper';
+import { ProgramTranslator } from './tree/program/program.translator';
 
 export function translateExpression(xonCode: string) {
     const tree = parseExpression(xonCode);
     const translator = getExpressionTranslator(tree);
+    return translator.translate();
+}
+
+export function translateProgram(xonCode: string) {
+    const tree = parseCode(xonCode, ProgramTree);
+    const translator = new ProgramTranslator(tree);
     return translator.translate();
 }
