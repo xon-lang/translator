8022d48565b1e12b3a5ccaec4eae84211a0f18ac
nizami <nizami.dev@gmail.com>
2020-08-24 21:43:10 +0300

    Update BitNotExpression translator

--- src/tree/expression/bit-and-expression.translator/bit-not-expression.test.ts
+++
@@ -1,9 +0,0 @@
-import { BitNotExpressionTree, parseCode } from '@xon/ast';
-import { getExpressionTranslator } from '../expression-helper';
-
-test('bit not', () => {
-    const code = '~234';
-    const tree = parseCode(code, BitNotExpressionTree);
-    const result = getExpressionTranslator(tree).translate();
-    expect(result).toBe('~234');
-});
