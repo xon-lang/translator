c0c5bcb1e36045f680915eb15f93a8e4cabbebda
nizami <nizami.dev@gmail.com>
2020-08-24 23:52:05 +0300

    Update Pow test

--- src/tree/expression/pow-expression/pow-expression.test.ts
+++ src/tree/expression/pow-expression/pow-expression.test.ts
@@ -2,5 +2,5 @@ import { translateExpression } from '../../../translate';
 
 test('pow', () => {
     const code = '25^ 30';
-    expect(translateExpression(code)).toBe('Math.pow(25, -30)');
+    expect(translateExpression(code)).toBe('Math.pow(25, 30)');
 });
