b95329a44f8fdf8345a059d170a034199c927673
nizami <nizami.dev@gmail.com>
2021-04-05 19:51:39 +0300

    Remove sandbox

--- sandbox/index.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { translateProgram } from '../src/translate';
-
-const dir = path.resolve(process.cwd(), 'sandbox');
-const code = fs.readFileSync(dir + '/program.xon').toString();
-fs.writeFileSync(dir + '/output.ts', translateProgram(code));
