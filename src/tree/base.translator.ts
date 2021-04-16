e0a6eed1ee992b9df5619efd54129ccfa03fa91e
nizami <nizami.dev@gmail.com>
2021-04-16 23:40:23 +0300

    Cleanup base translator

--- src/tree/base.translator.ts
+++ src/tree/base.translator.ts
@@ -1,21 +1,8 @@
 import { BaseTree } from '@xon/ast';
 
 export abstract class BaseTranslator {
-    static scopes = [];
-    static uniqueCounter = 0;
-
-    get scopes() {
-        return BaseTranslator.scopes;
-    }
-    get currentScope() {
-        return BaseTranslator.scopes[BaseTranslator.scopes.length - 1];
-    }
-
     tree: BaseTree;
 
     abstract translate();
 
-    getUniqueId() {
-        return '___id_' + BaseTranslator.uniqueCounter++;
-    }
 }
